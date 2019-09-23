eb93a512697023b6a141549871e999eaf8e41a38
nick <lfwang@g.ucla.edu>
2019-09-23 14:50:19 -0700

    delete copy file

--- Information_Bottleneck/cmd (copy).py
+++
@@ -1,8 +0,0 @@
-import os
-
-para="/Volumes/Macintosh\ HD\ -\ Data/Script/Information_Bottleneck/Script"
-os.system(para)
-
-#para_cmd="parallel --jobs 2 --delay 2 --progress"
-#para_tar=" \"./runner\" {1} ::: 1 2"
-#os.system(para_cmd+para_tar)
